b623d96f98097fd8ddde3b17516516b5e351ca97
Paul Biberstein <biberstein.paul@gmail.com>
2024-04-23 16:02:22 -0400

    Add functions from `flamescope` and FFI interface
    * copy minimal working code from flamescope
    * add FFI to allow calling flame functions from C

--- src/lib.rs
+++ src/lib.rs
@@ -335,9 +335,11 @@ pub fn start<S: Into<StrCow>>(name: S) {
         let id = collector.next_id;
         collector.next_id += 1;
 
+        let parent = collector.id_stack.last().cloned();
+
         let this = Event {
             id,
-            parent: collector.id_stack.last().cloned(),
+            parent,
             name: name.into(),
             collapse: false,
             start_ns: ns_since_epoch(epoch),
@@ -557,3 +559,225 @@ pub fn dump_json<W: std::io::Write>(out: &mut W) -> std::io::Result<()> {
 }
 
 pub use html::{dump_html, dump_html_custom};
+
+// ======================= flamescope ===============================
+
+mod flamescope {
+
+use super::Span;
+use super::StrCow;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SpeedscopeFile {
+    #[serde(rename = "$schema")]
+    pub schema: &'static str,
+
+    pub profiles: Vec<Profile>,
+    pub shared: Shared,
+
+    pub active_profile_index: Option<u64>,
+
+    pub exporter: Option<String>,
+
+    pub name: Option<String>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "camelCase")]
+pub enum Profile {
+    #[serde(rename_all = "camelCase")]
+    Sampled {
+        name: StrCow,
+        unit: ValueUnit,
+        start_value: u64,
+        end_value: u64,
+        samples: Vec<SampledStack>,
+        weights: Vec<u64>,
+    },
+    #[serde(rename_all = "camelCase")]
+    Evented {
+        name: StrCow,
+        unit: ValueUnit,
+        start_value: u64,
+        end_value: u64,
+        events: Vec<Event>,
+    },
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Event {
+    #[serde(rename = "type")]
+    pub event_type: EventType,
+    pub at: u64,
+    pub frame: usize,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub enum EventType {
+    #[serde(rename = "O")]
+    OpenFrame,
+    #[serde(rename = "C")]
+    CloseFrame,
+}
+
+type SampledStack = Vec<usize>;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Shared {
+    pub frames: Vec<Frame>,
+}
+
+#[derive(Debug, PartialEq, Clone, Eq, Hash, Serialize, Deserialize)]
+pub struct Frame {
+    pub name: StrCow,
+    pub file: Option<String>,
+    pub line: Option<u32>,
+    pub col: Option<u32>,
+}
+
+impl Frame {
+    #[inline]
+    pub fn new(name: StrCow) -> Frame {
+        Frame {
+            name,
+            file: None,
+            line: None,
+            col: None,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ValueUnit {
+    Bytes,
+    Microseconds,
+    Milliseconds,
+    Nanoseconds,
+    None,
+    Seconds,
+}
+
+extern crate indexmap;
+
+use self::indexmap::IndexSet;
+
+use std::io::Write;
+
+const JSON_SCHEMA_URL: &str = "https://www.speedscope.app/file-format-schema.json";
+
+/// Convert flame spans to the speedscope profile format.
+pub fn spans_to_speedscope(spans: Vec<Span>) -> SpeedscopeFile {
+    let mut frames = IndexSet::new();
+    let profiles = spans
+        .into_iter()
+        .map(|span| Profile::Evented {
+            name: span.name.clone(),
+            unit: ValueUnit::Nanoseconds,
+            start_value: span.start_ns,
+            end_value: span.end_ns,
+            events: {
+                let mut events = Vec::new();
+                span_extend_events(&mut frames, &mut events, span);
+                events
+            },
+        })
+        .collect();
+    SpeedscopeFile {
+        // always the same
+        schema: JSON_SCHEMA_URL,
+        active_profile_index: None,
+        exporter: None,
+        name: None,
+        profiles,
+        shared: Shared {
+            frames: frames.into_iter().collect(),
+        },
+    }
+}
+
+fn span_extend_events(frames: &mut IndexSet<Frame>, events: &mut Vec<Event>, span: Span) {
+    let (frame, _) = frames.insert_full(Frame::new(span.name));
+    events.push(Event {
+        event_type: EventType::OpenFrame,
+        at: span.start_ns,
+        frame,
+    });
+    for child in span.children {
+        span_extend_events(frames, events, child);
+    }
+    events.push(Event {
+        event_type: EventType::CloseFrame,
+        at: span.end_ns,
+        frame,
+    });
+}
+
+#[inline]
+pub fn dump(writer: impl Write) -> serde_json::Result<()> {
+    write_spans(writer, super::spans())
+}
+
+#[inline]
+pub fn write_spans(writer: impl Write, spans: Vec<Span>) -> serde_json::Result<()> {
+    let speedscope = spans_to_speedscope(spans);
+    serde_json::to_writer(writer, &speedscope)
+}
+}
+
+// ============================ FFI  ================================
+use std::ffi::CStr;
+use std::fs::File;
+use std::os::raw::c_char;
+
+#[no_mangle]
+pub extern fn flame_start(name: *const c_char) {
+    let result = std::panic::catch_unwind(|| {
+        let name = unsafe { CStr::from_ptr(name).to_str().unwrap().to_owned() };
+        start(name); 
+    });
+    if result.is_err() {
+        eprintln!("error: rust panicked");
+    }
+}
+
+#[no_mangle]
+pub extern fn flame_end(name: *const c_char) {
+    let result = std::panic::catch_unwind(|| {
+        let name = unsafe { CStr::from_ptr(name).to_str().unwrap().to_owned() };
+        end(name); 
+    });
+    if result.is_err() {
+        eprintln!("error: rust panicked");
+    }
+}
+
+#[no_mangle]
+pub extern fn flame_dump(path: *const c_char) {
+    let path = unsafe { CStr::from_ptr(path).to_str().unwrap() };
+    flamescope::dump(&mut File::create(path).unwrap()).unwrap();
+}
+
+#[no_mangle]
+pub extern fn flame_dump_html(path: *const c_char) {
+    let path = unsafe { CStr::from_ptr(path).to_str().unwrap() };
+    dump_html(&mut File::create(path).unwrap()).unwrap();
+}
+
+#[no_mangle]
+pub extern fn flame_debug() {
+    debug();
+}
+
+#[no_mangle]
+pub extern fn flame_dump_stdout() {
+    dump_stdout();
+}
+
+#[no_mangle]
+pub extern fn flame_clear() {
+    clear();
+}
+
